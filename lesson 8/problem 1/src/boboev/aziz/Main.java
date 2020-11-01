097be9de2b9bd1ccf1da30e804fc67f46a5b8a0a
Aziz Boboev <boboevaziz1303@gmail.com>
2020-11-01 15:23:49 +0500

    решено

--- lesson 8/problem 1/src/boboev/aziz/Main.java
+++ lesson 8/problem 1/src/boboev/aziz/Main.java
@@ -1,8 +1,14 @@
 package boboev.aziz;
+        public class Main {
+            public static void main(String[] args) {
+                int maxNum = max(218, 43);
+        System.out.println(maxNum);
+    }
 
-public class Main {
-
-    public static void main(String[] args) {
-
+    public static int max(int x, int y) {
+        if (x > y) {
+            return x;
+        } else {
+            return y; }
     }
 }
