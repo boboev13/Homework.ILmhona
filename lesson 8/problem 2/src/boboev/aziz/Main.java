1d16f0f3dbb4e01632ce66e17f9a4b82e228436a
Aziz Boboev <boboevaziz1303@gmail.com>
2020-11-01 15:42:52 +0500

    solved

--- lesson 8/problem 2/src/boboev/aziz/Main.java
+++ lesson 8/problem 2/src/boboev/aziz/Main.java
@@ -2,7 +2,15 @@ package boboev.aziz;
 
 public class Main {
 
-    public static void main(String[] args) {
-	
+            public static void main(String[] args) {
+            int total = SumOfSquares(1, 4);
+                System.out.println(total);
+        }
+        public static int SumOfSquares(int a, int b) {
+            int sum = 0;
+            for (int i = a; i <= b; i++) {
+                sum = sum + i * i;
+            }
+            return sum;
     }
 }
