dfc64b0d24295c7cb6ef507c058b71d79c2c30aa
Aziz Boboev <boboevaziz1303@gmail.com>
2020-11-07 13:22:50 +0500

    solved

---
+++ lesson 8/problem 5/src/boboev/aziz/Main.java
@@ -0,0 +1,17 @@
+package boboev.aziz;
+
+public class Main {
+
+    public static void main(String[] args) {
+            int[] array2 = {12, 2, 1, 8, 3, 9, 6};
+            int totalNumArray = total(array2);
+            System.out.println(totalNumArray);
+        }
+        public static int total (int [] array){
+            int counter = 0;
+            for ( int i = 0; i < array.length; i++){
+                counter += array[i];
+            }
+            return counter;
+    }
+}
